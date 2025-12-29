e84cf85347c001c032f6f99afa0417fd095487d8
Anshuman Rudra <anshuman.rudra@disney.com>
2025-12-29 12:22:14 -0500

    add launch w/ flicker sample

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/LaunchSample.java
@@ -0,0 +1,89 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Launch", group="Linear OpMode")
+public class LaunchSample extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor launch;
+    private CRServo flicker;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        launch = hardwareMap.get(DcMotor.class, "launch");
+        flicker = hardwareMap.get(CRServo.class, "flicker");
+
+        launch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        launch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launch.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to operate TeleOp" );
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
+            
+            // Launch motor control with left stick X
+            double launchPower = -gamepad1.left_stick_x;
+            launch.setPower(launchPower);
+            
+            // Flicker control with left bumper
+            double flickerP = gamepad1.left_trigger;
+            flickerP *= 10;
+            if (flickerP > 0) {
+                flicker.setPower(flickerP);
+            } else {
+                flicker.setPower(0);
+            }
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Launch Power", "%.2f", launchPower);
+            telemetry.addData("Flicker Power", "%.2f", flicker.getPower());
+            telemetry.addData(">", "Left Stick X: Launch, Left Bumper: Flicker" );
+            telemetry.update();
+        }
+    }
+}
