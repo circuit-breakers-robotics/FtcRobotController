cd647f1ea6553b83e51d39680c839492411a0ebd
Indumathi Balasubramaniam <indu@Indumathis-MacBook-Pro.local>
2025-12-31 12:46:33 -0500

    add auto run near launch zone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircuitBreakersAutoRunNearLaunch.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="31250_Auto_Near_Launch", group = "Auto")
+public class CircuitBreakersAutoRunNearLaunch {
+}
