44c7b5701d69bab803e83c338faa1c2963e12d70
Anshuman Rudra <anshuman.rudra@disney.com>
2026-01-04 23:39:18 -0500

    add integration test teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/IntrgratedRobotTeleOpSample.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "IntrgratedRobotTeleOpSample", group = "TeleOp")
+public class IntrgratedRobotTeleOpSample extends LinearOpMode {
+
+    CRServo flicker;
+    CRServo spindexer;
+    DcMotor intake;
+    DcMotor launch;
+
+    @Override
+    public void runOpMode() {
+        flicker = hardwareMap.get(CRServo.class, "flicker");
+        spindexer = hardwareMap.get(CRServo.class, "spindexer");
+        spindexer.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        intake.setDirection(DcMotor.Direction.REVERSE);
+
+        launch = hardwareMap.get(DcMotor.class, "launch");
+        launch.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        launch.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to scan Servo." );
+        telemetry.update();
+        waitForStart();
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
+            runIntake();
+            runFlicker();
+            runSpindexer();
+            runLaunch();
+            telemetry.update();
+        }
+
+        // Signal done;
+        telemetry.addData(">", "Done");
+        telemetry.update();
+    }
+
+    private void runFlicker() {
+        double flickerPower = gamepad2.right_trigger;
+        flickerPower *= 20;
+
+        if(flickerPower > 0) {
+            flicker.setPower(flickerPower);  // Use trigger value as power (0-1)
+        } else {
+            flicker.setPower(0);  // Stop servo
+        }
+        telemetry.addData("Flicker Power: ", flickerPower);
+    }
+
+    private void runSpindexer() {
+        double spindexerPower = gamepad2.left_trigger;
+        spindexerPower *= 20;
+        if(spindexerPower > 0) {
+            spindexer.setPower(spindexerPower);  // Use trigger value as power (0-1)
+        } else {
+            spindexer.setPower(0);
+        }
+        telemetry.addData("Spindexer Power: ", spindexerPower);
+    }
+
+    private void runLaunch() {
+        double launchPower = -gamepad1.left_stick_y;
+        launch.setPower(launchPower);
+        telemetry.addData("Launch Power: ", launchPower);
+    }
+
+    private void runIntake() {
+        double intakePower = -gamepad1.right_stick_y;
+        intake.setPower(intakePower);
+        telemetry.addData("Intake Power: ", intakePower);
+    }
+}
